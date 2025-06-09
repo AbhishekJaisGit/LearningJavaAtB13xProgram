2b2e669f610899681efc6ed1d8955646d8757b32
AbhishekJaisGit <abhishekjais101@gmail.com>
2025-06-09 22:36:46 +0530

    New Commit 1

---
+++ src/ex_01_Java_Basic/Lab003_Comments.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basic;
+
+public class Lab003_Comments {
+        public static void main(String[] args) {
+            System.out.println("Hi! \nI am trying comments in my program\n");
+
+            //Single Line comment
+            System.out.println("Tried Single line comments");
+
+        /* Multiple Line comment
+        line 2
+        line 3
+         */
+            System.out.println("Tried Multiple line comments");
+        }
+}
