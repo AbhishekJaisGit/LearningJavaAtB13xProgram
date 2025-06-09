6fd92588e09c037fd9c6856a077fd53ebb8df7a8
AbhishekJaisGit <abhishekjais101@gmail.com>
2025-06-09 23:02:22 +0530

    New Commit 3

---
+++ src/ex_01_Java_Basic/Lab004_Keywords.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basic;
+
+public class Lab004_Keywords {
+    public static void main(String[] args) {
+        /*
+         * Author : Abhishek Jaiswal
+         * Course :  ATB13x
+         * This program creates a list of all the keywords in this program
+         */
+
+        // List of keywords
+        System.out.println("List of Keywords:\npackage, public, class, static, void");
+
+        // Total count of keywords
+        System.out.println("Total count of keywords = 5");
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab005_Identifiers.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basic;
+
+public class Lab005_Identifiers {
+    public static void main(String[] args) {
+        /*
+         * Author : Abhishek Jaiswal
+         * Course :  ATB13x
+         * This program creates a list of all the identifiers in this program
+         */
+
+        // List of keywords
+        System.out.println("List of Identifiers:\nLab005_Identifiers, args");
+
+        // Total count of keywords
+        System.out.println("Total count of identifiers = 2");
+    }
+}
